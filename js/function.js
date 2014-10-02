7d6c15ba1e362a7ffbde2f6f6356be55d64bbeab
elgildegerard <elgildegerard@MacBook-de-Gerardo-Gil.local>
2014-10-02 18:43:10 -0300

    +

--- js/function.js
+++ js/function.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
 
     //CREO VARIABLES DE LOS ELEMENTOS
-    var $parrafo = $(".paragraph");
-    var $header = $("ul li a")
+    var parrafo = $(".paragraph");
+    var header = $("ul li a");
     
     
     //OCULTO PÁRRAFO
-    $parrafo.hide();
+    parrafo.hide();
     
     //MUESTRO EL PRIMERO
     $('.paragraph:first').show();
     
     //ON CLICK
-    $header.click(function(e){
-        e.preventDefault();
-        $parrafo.slideUp("$parrafo");
-        $(this).siblings().slideDown("$parrafo");
+    header.click(function(e){
+    e.preventDefault();
+    parrafo.slideUp();
+    $(this).siblings(".paragraph").slideDown();
         
-        $header.removeClass(".active");
-        $(this).addClass(".active");
+        header.removeClass("active");
+        $(this).addClass("active");
         
     });
     
